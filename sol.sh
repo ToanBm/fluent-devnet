37d9d49011ac38da23aea9da64694d390be0f8e2
toanbm85 <94586777+ToanBm@users.noreply.github.com>
2025-04-09 22:36:26 +0700

    Update sol.sh

--- sol.sh
+++ sol.sh
@@ -22,24 +22,30 @@ foundryup
 # Start Foundry Project
 forge init --force
 
+# Set contract:
+read -p "Enter token symbol (e.g., FLN): " TOKEN_SYMBOL
+read -p "Enter total supply (e.g., 1000000): " TOTAL_SUPPLY
+
 # Start Solidity Contract
 rm src/Counter.sol
 
-cat <<'EOF' > src/Contract.sol
+cat <<EOF > src/Contract.sol
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
 contract SimpleStorage {
 
-    uint256 public storedData; //Do not set 0 manually it wastes gas!
+    uint256 public storedData; // Do not set 0 manually it wastes gas!
+
+    string public symbol = "$TOKEN_SYMBOL";
+    uint256 public totalSupply = $TOTAL_SUPPLY;
 
     event setEvent();
-    
+
     function set(uint256 x) public {
         storedData = x;
         emit setEvent();
     }
-
 }
 EOF
 
