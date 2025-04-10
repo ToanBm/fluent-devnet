414b0eb0237c498be79356695fab54ff0ea5d8ae
toanbm85 <94586777+ToanBm@users.noreply.github.com>
2025-04-10 15:00:28 +0700

    Update msol.sh

--- msol.sh
+++ msol.sh
@@ -22,24 +22,36 @@ foundryup
 # Start Foundry Project
 forge init --force
 
+# Set contract:
+# Random symbol
+TOKEN_SYMBOL=$(tr -dc A-Z </dev/urandom | head -c 3)
+
+# Random total supply (1_000_000 → 9_000_000)
+TOTAL_SUPPLY=$(( (RANDOM % 9 + 1) * 1000000 ))
+
+echo "🪙 Symbol: $TOKEN_SYMBOL"
+echo "💰 Total Supply: $TOTAL_SUPPLY"
+
 # Start Solidity Contract
 rm src/Counter.sol
 
-cat <<'EOF' > src/Contract.sol
+cat <<EOF > src/Contract.sol
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
 contract SimpleStorage {
 
-    uint256 public storedData; //Do not set 0 manually it wastes gas!
+    uint256 public storedData; // Do not set 0 manually it wastes gas!
+
+    string public symbol = "$TOKEN_SYMBOL";
+    uint256 public totalSupply = $TOTAL_SUPPLY;
 
     event setEvent();
-    
+
     function set(uint256 x) public {
         storedData = x;
         emit setEvent();
     }
-
 }
 EOF
 
